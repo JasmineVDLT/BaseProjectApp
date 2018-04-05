4fe4b326975f2306283b6f57e73d35cb5b0d0926
Jasmine De La Torre <valverde@sonoma.edu>
2018-04-04 18:06:43 -0700

    04/04/18
    added a splash page

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.sp18.ssu370.baseprojectapp.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.sp18.ssu370.baseprojectapp.R;
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MAIN", "onCreate: Main activity");
     }
 }
